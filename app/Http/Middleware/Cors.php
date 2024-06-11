77b352d44ec04599795eb44c905f4f3ef41e72b8
suraj jaiswal <jaiswaljesus384@gmail.com>
2024-06-11 22:25:29 +0530

    cross rogin problem resolved

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,12 +8,21 @@ class Cors
 {
   public function handle($request, Closure $next)
   {
+    $headers = [
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Max-Age' => '86400',
+      'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
+    ];
+    
+    if ($request->isMethod('OPTIONS')) {
+      return response()->json('{"method":"OPTIONS"}', 200, $headers);
+    }
+    
     $response = $next($request);
-
-    if (method_exists($response, 'header')) {
-      $response->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    foreach ($headers as $key => $value) {
+      $response->header($key, $value);
     }
 
     return $response;
