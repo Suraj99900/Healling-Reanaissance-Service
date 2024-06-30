7b55f033fbad354bd4e493d2e681e4204f980754
suraj jaiswal <jaiswaljesus384@gmail.com>
2024-06-30 17:40:03 +0530

    added new changes

--- app/Models/AppPost.php
+++ app/Models/AppPost.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query;
+use Illuminate\Support\Facades\DB;
 
 class AppPost extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = "app_post";
 
     protected $fillable = [
         'id',
