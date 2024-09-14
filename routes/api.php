215f9b5de341aef0b8d19985ece4cd3d766a1a7a
suraj jaiswal <jaiswaljesus384@gmail.com>
2024-09-14 20:56:56 +0530

    Web-Testing-changes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppPostController;
+use App\Http\Controllers\AttachmentFileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EmailController;
+use App\Http\Controllers\UserCommentController;
+use App\Http\Controllers\VideoController;
+use App\Http\Controllers\VideoCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +21,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('ensure.token.is.valid')->group(function () {
+    // User Register and Login
+    Route::get('login', [UserController::class, 'login']);
+    Route::post('users', [UserController::class, 'addUser']);
+    Route::put('password', [UserController::class, 'updatePassword']);
+    Route::put('users/{id}', [UserController::class, 'update']);
+    Route::delete('users/{id}', [UserController::class, 'destroy']);
+
+    // Email route
+    Route::post('email', [EmailController::class, 'generateMail']);
+
+    // Video upload
+    Route::post('video', [VideoController::class, 'upload']);
+    Route::get('video/{id}', [VideoController::class, 'fetchById']);
+    Route::get('videos', [VideoController::class, 'fetchAll']);
+    Route::get('videos-category/{id}', [VideoController::class, 'fetchAllVideoDataByCategoryId']);
+    Route::get('videos/paginated', [VideoController::class, 'fetchAllWithPagination']);
+    Route::get('videos/search', [VideoController::class, 'searchByTitle']);
+    Route::put('video/{id}', [VideoController::class, 'update']);
+    Route::delete('video/{id}', [VideoController::class, 'destroy']);
+    Route::get('stream/{id}', [VideoController::class, 'stream']);
+    Route::get('thumbnail/{id}', [VideoController::class, 'thumbnailImages']);
+
+    // Video Category
+    Route::post('video-category', [VideoCategoryController::class, 'addCategory']);
+    Route::get('video-categories', [VideoCategoryController::class, 'getAllCategories']);
+    Route::get('video-category/{id}', [VideoCategoryController::class, 'getCategoryById']);
+    Route::put('video-category/{id}', [VideoCategoryController::class, 'updateCategory']);
+    Route::delete('video-category/{id}', [VideoCategoryController::class, 'deleteCategory']);
+
+    // Attachment route
+    Route::post('app-attachment', [AttachmentFileController::class, 'addAttchmentData']);
+    Route::get('video/app-attachment/{id}', [AttachmentFileController::class, 'fetchAllAttachmentDataByVideoId']);
+    Route::delete('app-attachment/{id}', [AttachmentFileController::class, 'removedAttchment']);
+
+    // Video Comment 
+    Route::post('video/comment', [UserCommentController::class, 'addUserComment']);
+    Route::put('video/comment', [UserCommentController::class, 'updateUserComment']);
+    Route::get('video/comment/{id}', [UserCommentController::class, 'fetchUserCommentByVideoId']);
+    Route::get('video/comment', [UserCommentController::class, 'fetchAllUserComment']);
+    Route::delete('video/comment/{id}', [UserCommentController::class, 'invalidUserCommentById']);
+
+    // Blog post API
+    Route::post('blog', [AppPostController::class, 'store']);
+    Route::put('blog/{id}', [AppPostController::class, 'update']);
+    Route::get('blog', [AppPostController::class, 'index']);
+    Route::get('blog/{id}', [AppPostController::class, 'show']);
+    Route::delete('blog', [AppPostController::class, 'destroy']);
 });
