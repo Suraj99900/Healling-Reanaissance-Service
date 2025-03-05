d2f2a71a9dfe5a48ff7631998fc2e24dd0780846
suraj jaiswal <jaiswaljesus384@gmail.com>
2025-03-06 00:27:56 +0530

    Added admin Panel

--- resources/views/CDN_Header.blade.php
+++ resources/views/CDN_Header.blade.php
@@ -3,38 +3,69 @@
 
 <head>
 
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Title -->
-    <title>Smart Place || Showcasing Expertise and Insights</title>
+    <title>Life Healer Kavita - 10X Your Power of Manifestation</title>
 
     <!-- Favicon -->
-    <link rel="icon" href="{{asset('img/DashboardNewImage-transparent.png')}}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{asset('img/DashboardNewImage-transparent.png')}}" type="image/x-icon">
+    <link rel="icon" href="https://lifehealerkavita.com/res/img/LifeHealer/appicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://lifehealerkavita.com/res/img/LifeHealer/appicon.png" type="image/x-icon">
 
     <!-- SEO Meta Tags -->
     <meta name="description"
-        content="Explore my portfolio showcasing expertise, insights, and skills in [your specialties]. Dive into my blog for valuable insights, tutorials, and industry updates.">
+        content="Life Healer offers holistic health solutions to achieve physical, mental, and spiritual wellness. Join us for a healthier you.">
     <meta name="keywords"
-        content="portfolio, blogging, personal branding, [specific skills], [industry topics],PHP,Development, career growth, insights, tutorials">
+        content="holistic health, wellness, life healing, spiritual wellness, mental health, physical health, manifestation, career growth, success">
     <meta name="robots" content="index, follow">
-    <link rel="canonical" href="https://smartpoly.myportfolio.com">
+    <link rel="canonical" href="https://lifehealerkavita.com">
 
     <!-- Open Graph Meta Tags -->
-    <meta property="og:title" content="Smart Place - Showcasing Expertise and Insights">
+    <meta property="og:title" content="Life Healer Kavita - 10X Your Power of Manifestation">
     <meta property="og:description"
-        content="A portfolio that highlights skills and insights, along with a blog featuring tutorials and industry trends.">
-    <meta property="og:image" content="{{asset('img/PortfolioIcon.png')}}">
-    <meta property="og:url" content="https://smartpoly.myportfolio.com">
+        content="Life Healer offers holistic health solutions to achieve physical, mental, and spiritual wellness. Join us for a healthier you.">
+    <meta property="og:image" content="https://lifehealerkavita.com/res/img/LifeHealer/appicon.png">
+    <meta property="og:url" content="https://lifehealerkavita.com">
     <meta property="og:type" content="website">
 
     <!-- Twitter Card Meta Tags -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Smart Place - Portfolio & Blog">
+    <meta name="twitter:card" content="Life Healer Kavita - 10X Your Power of Manifestation">
+    <meta name="twitter:title" content="Life Healer Kavita - 10X Your Power of Manifestation">
     <meta name="twitter:description"
-        content="Explore my portfolio and read my blog for the latest tutorials and insights in [industry].">
-    <meta name="twitter:image" content="{{asset('img/DashboardNewImage-transparent.png')}}">
+        content="Life Healer offers holistic health solutions to achieve physical, mental, and spiritual wellness. Join us for a healthier you.">
+    <link rel="shortcut icon" href="https://lifehealerkavita.com/res/img/LifeHealer/appicon.png" type="image/x-icon"
+        id="ff_favicon" />
+    <meta name="twitter:image" content="https://lifehealerkavita.com/res/img/LifeHealer/appicon.png">
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="https://lifehealerkavita.com" />
+    <meta property="og:url" content="https://lifehealerkavita.com" />
+    <meta itemprop="url" content="https://lifehealerkavita.com" />
+    <meta name="twitter:url" content="https://lifehealerkavita.com" />
+
+    <meta name="author" content="Kavita">
+    <meta name="site_id" content="lifehealerkavita.com/">
+    <meta name="LifeHealer_id" content="https://lifehealerkavita.com/LifeHealer/index.php">
+    <meta name="LifeHealer_page_id" content="https://lifehealerkavita.com/LifeHealer/index.php">
+    <meta name="pageType" content="default">
+
+    <!-- CDNS -->
+    <!-- Bootstrap 5 link -->
+    <link rel="stylesheet" href="../res/cdn.jsdelivr.net_npm_bootstrap@5.3.2_dist_css_bootstrap.min.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
+    <!-- ICONSCOUT CDN -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+    <!-- DataTable CDN -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
+
+    <!-- razorpay CDN -->
+    <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Hand:wght@500&display=swap" rel="stylesheet">
 
     <!-- CDNS -->
     <!-- Bootstrap 5 link -->
@@ -68,4 +99,4 @@
 </head>
 
 <body class="dark">
-<!-- <canvas id="particles"></canvas> -->
+    <!-- <canvas id="particles"></canvas> -->
